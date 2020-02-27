42a7ad27bb54019677d4c56666b1d73199204816
hoapc <hoabingbong94@gmail.com>
2020-02-27 09:05:26 +0700

    update package

--- views/setup.php
+++ views/setup.php
@@ -1,2 +1,5 @@
-<script type="text/javascript" src="<?php echo asset('js/ckfinder/ckfinder.js') ?>"></script>
+<?php
+$url = config('ckfinder.urlPrefix');
+?>
+<script type="text/javascript" src="<?php echo $url . '/js/ckfinder/ckfinder.js' ?>"></script>
 <script>CKFinder.config( { connectorPath: <?php echo json_encode(route('ckfinder_connector')) ?> } );</script>
